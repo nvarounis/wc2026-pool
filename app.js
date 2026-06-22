577224b8d73a5c8418c49562019a27709c1966d2
nvarounis <nvarounis@gmail.com>
2026-06-22 11:35:43 +0300

    Fetch and log CSV data from Google Sheets
    Added functionality to fetch and log CSV data from a Google Sheets URL.

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("WC2026 Pool loaded");
+const csvUrl =
+"https://docs.google.com/spreadsheets/d/e/2PACX-1vR1iFv4TIjSckPSECiJWeS3BF59LWaefQam-qr7j4LoswG0_9g2WzlePEVrBamMQqmN3_Hpoa1g-SQX/pub?gid=1488246594&single=true&output=csv";
+
+fetch(csvUrl)
+  .then(response => response.text())
+  .then(data => {
+      console.log("CSV Loaded");
+      console.log(data.substring(0,500));
+  })
+  .catch(error => console.error(error));
