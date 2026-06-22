e3e9ad6973b9dc931c48904a87c97230090d4fbd
nvarounis <nvarounis@gmail.com>
2026-06-22 12:24:43 +0300

    Refactor CSV processing to use forEach
    Refactor CSV fetching and processing logic to use forEach for better readability.

--- app.js
+++ app.js
@@ -2,34 +2,18 @@ const csvUrl =
 "https://docs.google.com/spreadsheets/d/e/2PACX-1vR1iFv4TIjSckPSECiJWeS3BF59LWaefQam-qr7j4LoswG0_9g2WzlePEVrBamMQqmN3_Hpoa1g-SQX/pub?gid=1488246594&single=true&output=csv";
 
 fetch(csvUrl)
-.then(response => response.text())
-.then(csv => {
+  .then(response => response.text())
+  .then(csv => {
 
     const rows = csv.split("\n");
 
-    let html = "";
+    const firstRow = rows[0].split(",");
 
-    for(let i = 1; i < rows.length; i++){
+    firstRow.forEach((col, i) => {
+      console.log(i, col);
+    });
 
-        const cols = rows[i].split(",");
+    alert("Check Console");
 
-        const rank = cols[18];
-        const player = cols[19];
-        const pts = cols[20];
-
-        if(rank && player && pts){
-
-            html += `
-            <tr>
-                <td>${rank}</td>
-                <td>${player}</td>
-                <td align="right">${pts}</td>
-            </tr>
-            `;
-        }
-    }
-
-    document.getElementById("leaderboard-body").innerHTML = html;
-
-})
-.catch(error => console.error(error));
+  })
+  .catch(error => console.error(error));
