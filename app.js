523e7e74711d5051b52b1f0c8eea3c8c29c325bc
nvarounis <nvarounis@gmail.com>
2026-06-22 11:59:51 +0300

    Add alert for app load notification

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+alert("APP LOADED");
 const csvUrl =
 "https://docs.google.com/spreadsheets/d/e/2PACX-1vR1iFv4TIjSckPSECiJWeS3BF59LWaefQam-qr7j4LoswG0_9g2WzlePEVrBamMQqmN3_Hpoa1g-SQX/pub?gid=1488246594&single=true&output=csv";
 
