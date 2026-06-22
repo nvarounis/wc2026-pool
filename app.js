cf6f0d69223ce416b7bcefeea79c528e10f11d4c
nvarounis <nvarounis@gmail.com>
2026-06-22 12:13:31 +0300

    Refactor CSV loading and logging
    Removed alert and console logs, added header logging.

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-alert("APP LOADED");
-
 const csvUrl =
 "https://docs.google.com/spreadsheets/d/e/2PACX-1vR1iFv4TIjSckPSECiJWeS3BF59LWaefQam-qr7j4LoswG0_9g2WzlePEVrBamMQqmN3_Hpoa1g-SQX/pub?gid=1488246594&single=true&output=csv";
 
@@ -7,17 +5,13 @@ fetch(csvUrl)
 .then(response => response.text())
 .then(csv => {
 
-    console.log("CSV LOADED");
-
     const rows = csv.split("\n");
 
-    console.log("ROWS:", rows.length);
+    // Βρες τη στήλη που περιέχει "ΘΕΣΗ"
+    const header = rows[0].split(",");
 
-    rows.slice(0,5).forEach((row,index)=>{
-        console.log(index,row);
-    });
+    console.log(header);
 
-})
-.catch(error => {
-    console.error("ERROR:", error);
+    alert("Check Console");
+
 });
