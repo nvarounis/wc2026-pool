88bffd6225467374ab79ef7cb4d961fba3d2cce8
nvarounis <nvarounis@gmail.com>
2026-06-22 12:03:51 +0300

    Load CSV data from Google Sheets
    Fetch and log CSV data from a Google Sheets URL.

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 alert("APP LOADED");
+
+const csvUrl =
+"https://docs.google.com/spreadsheets/d/e/2PACX-1vR1iFv4TIjSckPSECiJWeS3BF59LWaefQam-qr7j4LoswG0_9g2WzlePEVrBamMQqmN3_Hpoa1g-SQX/pub?gid=1488246594&single=true&output=csv";
+
+fetch(csvUrl)
+.then(response => response.text())
+.then(csv => {
+
+    console.log("CSV LOADED");
+
+    const rows = csv.split("\n");
+
+    console.log("ROWS:", rows.length);
+
+    rows.slice(0,5).forEach((row,index)=>{
+        console.log(index,row);
+    });
+
+})
+.catch(error => {
+    console.error("ERROR:", error);
+});
